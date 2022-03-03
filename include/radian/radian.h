766588eab933cae678df9482b4a3a276fb17f8a5
raddari <harveywyatt@hotmail.com>
2022-03-03 13:51:34 +1030

    Add testing and header framework

--- include/radian/radian.h
+++ include/radian/radian.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include "radian-internal.h"
+
+
+#define RADIAN_READ(file, dest)
+#define RADIAN_READ_MANY(file, dest, n)
+#define RADIAN_READ_BE(file, dest)
+#define RADIAN_READ_LE(file, dest)
+
+#define RADIAN_WRITE(file, src)
+#define RADIAN_WRITE_MANY(file, src, n)
+#define RADIAN_WRITE_BE(file, src)
+#define RADIAN_WRITE_LE(file, src)
