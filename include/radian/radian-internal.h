043ad65bc2f2e5d8f9ce79c1ca6df6550cebd558
raddari <harveywyatt@hotmail.com>
2022-03-04 01:59:50 +1030

    Add reading macros

--- include/radian/radian-internal.h
+++ include/radian/radian-internal.h
@@ -1,1 +1,47 @@
 #pragma once
+
+#include <stdio.h>
+
+
+#if !defined(RADIAN_HOST_BE) && !defined(RADIAN_HOST_LE)
+  #error Host endianness is undefined
+#endif
+
+
+#define _RADIAN_BSWAP(target)
+
+#define _RADIAN_READ_MANY(file, dest, n)                         \
+    {                                                            \
+      size_t nread = fread((dest), sizeof *(dest), (n), (file)); \
+      if (nread < (n)) {                                         \
+        if (feof(file)) {                                        \
+          fprintf(stderr, "End of file reading " #dest);         \
+        }                                                        \
+        if (ferror(file)) {                                      \
+          fprintf(stderr, "Error reading file " #dest);          \
+        }                                                        \
+      }                                                          \
+    }
+
+#define _RADIAN_READ(file, dest) _RADIAN_READ_MANY(file, &dest, 1)
+
+#ifdef RADIAN_HOST_LE
+  #define _RADIAN_READ_LE(file, dest) _RADIAN_READ(file, dest)
+  #define _RADIAN_READ_BE(file, dest) \
+      {                               \
+        _RADIAN_READ(file, dest);     \
+        _RADIAN_BSWAP(dest);          \
+      }
+#else
+  #define _RADIAN_READ_BE(file, dest) _RADIAN_READ(file, dest)
+  #define _RADIAN_READ_LE(file, dest) \
+      {                               \
+        _RADIAN_READ(file, dest);     \
+        _RADIAN_BSWAP(dest);          \
+      }
+#endif
+
+#define _RADIAN_WRITE_MANY(file, src, n)
+#define _RADIAN_WRITE(file, src)
+#define _RADIAN_WRITE_BE(file, src)
+#define _RADIAN_WRITE_LE(file, src)
--- include/radian/radian.h
+++ include/radian/radian.h
@@ -3,12 +3,12 @@
 #include "radian-internal.h"
 
 
-#define RADIAN_READ(file, dest)
-#define RADIAN_READ_MANY(file, dest, n)
-#define RADIAN_READ_BE(file, dest)
-#define RADIAN_READ_LE(file, dest)
+#define RADIAN_READ_MANY(file, dest, n) _RADIAN_READ_MANY(file, dest, n)
+#define RADIAN_READ(file, dest)         _RADIAN_READ(file, dest)
+#define RADIAN_READ_BE(file, dest)      _RADIAN_READ_BE(file, dest)
+#define RADIAN_READ_LE(file, dest)      _RADIAN_READ_LE(file, dest)
 
-#define RADIAN_WRITE(file, src)
-#define RADIAN_WRITE_MANY(file, src, n)
-#define RADIAN_WRITE_BE(file, src)
-#define RADIAN_WRITE_LE(file, src)
+#define RADIAN_WRITE_MANY(file, src, n) _RADIAN_WRITE_MANY(file, src, n)
+#define RADIAN_WRITE(file, src)         _RADIAN_WRITE(file, src)
+#define RADIAN_WRITE_BE(file, src)      _RADIAN_WRITE_BE(file, src)
+#define RADIAN_WRITE_LE(file, src)      _RADIAN_WRITE_LE(file, src)
