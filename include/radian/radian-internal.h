3a85ec7be663a02d3988c5eea47cfa9b5cd11648
raddari <harveywyatt@hotmail.com>
2022-03-05 01:52:59 +1030

    Add diagnostic to error messages

--- include/radian/radian-internal.h
+++ include/radian/radian-internal.h
@@ -8,19 +8,23 @@
 #endif
 
 
+#define _RADIAN_S_(x) #x
+#define _RADIAN_S(x) _RADIAN_S_(x)
+#define _RADIAN_DIAG _RADIAN_S(__FILE__) ":" _RADIAN_S(__LINE__) ": "
+
 #define _RADIAN_BSWAP(target)
 
-#define _RADIAN_READ_MANY(file, dest, n)                         \
-    {                                                            \
-      size_t nread = fread((dest), sizeof *(dest), (n), (file)); \
-      if (nread < (n)) {                                         \
-        if (feof(file)) {                                        \
-          fprintf(stderr, "End of file reading " #dest);         \
-        }                                                        \
-        if (ferror(file)) {                                      \
-          fprintf(stderr, "Error reading file " #dest);          \
-        }                                                        \
-      }                                                          \
+#define _RADIAN_READ_MANY(file, dest, n)                                   \
+    {                                                                      \
+      size_t nread = fread((dest), sizeof *(dest), (n), (file));           \
+      if (nread < (n)) {                                                   \
+        if (feof(file)) {                                                  \
+          fprintf(stderr, _RADIAN_DIAG "End of file reading " #dest "\n"); \
+        }                                                                  \
+        if (ferror(file)) {                                                \
+          fprintf(stderr, _RADIAN_DIAG "Error reading file " #dest "\n");  \
+        }                                                                  \
+      }                                                                    \
     }
 
 #define _RADIAN_READ(file, dest) _RADIAN_READ_MANY(file, &dest, 1)
